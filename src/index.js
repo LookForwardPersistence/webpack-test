27ac236808a943e192a0838029fa39d954513334
fqh <fqhua_5@sina.cn>
2018-05-28 21:06:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import _ from 'lodash'
-import './style.css'
-import icon from  './company.jpg'
-import Data from './data.xml'
-import printMe from './print'
-import cube from  './math'
+// import _ from 'lodash'
+import './style.css';
+import icon from  './company.jpg';
+import Data from './data.xml';
+import printMe from './print';
+import cube from  './math';
 function component() {
     var element = document.createElement('div');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML ="立方："+ cube(5).join("\n\n") //_.join(['Come on', 'webpack','Well','Greate'], ' ');
+    element.innerHTML =  "立方：" + cube(5) + "\n\n"; //_.join(['Come on', 'webpack','Well','Greate'], ' ');
     element.classList.add('purple')
 
     //将图片添加到div
